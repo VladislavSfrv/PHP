c5630e61ba7c0e5bee7abf5e024daaa52ab6aa0f
Vladislav <vladislavsafarov04@mail.ru>
2024-10-06 19:26:16 +0300

    Lesson 1 Done

--- lesson_1/code/index.php
+++ lesson_1/code/index.php
@@ -1,7 +1,7 @@
 <?php
-    $a = 5;
-    $b = '05';
-    var_dump($a == $b);
-    var_dump((int)'012345');
-    var_dump((float)123.0 === (int)123.0);
-    var_dump(0 == 'hello, world');
+    $a = 5; //Присвоение int переменной
+    $b = '05'; // Присвоение str переменной
+    var_dump($a == $b); // не строгое сравнение двух переменных, str приводится к int. Вывод true
+    var_dump((int)'012345'); // Перевод str в int. Вывод 12345
+    var_dump((float)123.0 === (int)123.0); // строгое сравнение int и float. Так как сравниваются и типы данных, вывод false
+    var_dump(0 == 'hello, world'); // Сравнение int и str. вывод false
